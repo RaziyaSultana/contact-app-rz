2493ab00022c4eebdf48f746b730f8bbe3338c39
RaziyaSultana <raziyasultana1997@gmail.com>
2022-03-01 16:12:10 +0530

    react- props, states, hooks, local storage.. committed

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -11,7 +11,9 @@ const ContactCard = (props) => {
                     <div>{email}</div>
                </div>
                <i className="trash alternate outline icon"
-               style={{color:"red", marginTop: "7px"}}>
+               style={{color:"red", marginTop: "7px"}}
+               onClick={() => props.clickhandler(id)}
+               >
                 </i>
            </div>
     );
